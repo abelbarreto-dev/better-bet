30aa54982cf3928443952ee24a4faf5ad1c721c4
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-05-11 19:32:07 -0300

    feat: patchs login single multi and get filter single

--- src/api/resource/api_resource.py
+++ src/api/resource/api_resource.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from typing import Any
+
+from datetime import datetime
 
 from src.api.routes.routes import ROUTES
 
@@ -33,3 +35,23 @@ def post_single_bet(single_bet: SingleBet) -> Any:
 @ROUTER.post(ROUTES["post_multi_bet"])
 def post_multi_bet(multi_bet: MultiBet) -> Any:
     return multi_bet
+
+
+@ROUTER.patch(ROUTES["patch_login_passwd"])
+def patch_login_passwd(login_passwd: Request) -> Any:
+    return login_passwd
+
+
+@ROUTER.patch(ROUTES["patch_single_bet"])
+def patch_single_bet(single_bet: Request) -> Any:
+    return single_bet
+
+
+@ROUTER.patch(ROUTES["patch_multi_bet"])
+def patch_multi_bet(multi_bet: Request) -> Any:
+    return multi_bet
+
+
+@ROUTER.get(ROUTES["get_filter_single"])
+def get_filter_single(date_from: datetime, date_to: datetime) -> Any:
+    return None
