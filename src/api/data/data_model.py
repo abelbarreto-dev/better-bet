d76c97c4e813fc0c79de6fe666f1c62417f386c2
Abel Carvalho <64908368+abelbarreto-dev@users.noreply.github.com>
2023-05-11 18:48:22 -0300

    feat: craete data models to orm (#5)
    * feat: configuration of database connection tools
    
    * feat: data model created to database

---
+++ src/api/data/data_model.py
@@ -0,0 +1,63 @@
+from peewee import (
+    Model,
+    AutoField,
+    CharField,
+    DateTimeField,
+    DecimalField,
+    ForeignKeyField,
+    TextField
+)
+
+from src.utils.connection import Database
+
+
+class Login(Model):
+    id = AutoField(null=False, primary_key=True)
+    player_name = CharField(max_length=128, null=False)
+    username = CharField(max_length=128, null=False)
+    password = CharField(max_length=512, null=False)
+
+    class Meta:
+        database = Database.connect
+
+
+class SingleBet(Model):
+    id = AutoField(null=False, primary_key=True)
+    id_login = ForeignKeyField(Login, Login.id, null=False)
+    home_team = CharField(max_length=128, null=False)
+    away_team = CharField(max_length=128, null=False)
+    team_bet = CharField(max_length=128, null=False)
+    odd = DecimalField(decimal_places=3, null=False)
+    value_invest = DecimalField(decimal_places=2, null=False)
+    profit = DecimalField(decimal_places=2, null=True)
+    potential_earnings = DecimalField(decimal_places=2, null=True)
+    total_amount = DecimalField(decimal_places=2, null=True)
+    bet_status = CharField(max_length=64, null=True)
+    description = CharField(max_length=256, null=True)
+    create_datetime = DateTimeField(null=False)
+    finish_datetime = DateTimeField(null=True)
+    operator_fee = DecimalField(decimal_places=2, null=True)
+
+    class Meta:
+        database = Database.connect
+
+
+class MultiBet(Model):
+    id = AutoField(null=False, primary_key=True)
+    id_login = ForeignKeyField(Login, Login.id, null=False)
+    home_team = CharField(max_length=128, null=False)
+    away_team = CharField(max_length=128, null=False)
+    team_bet = CharField(max_length=128, null=False)
+    value_invest = DecimalField(decimal_places=2, null=False)
+    multi_odds = TextField(null=True)
+    profit = DecimalField(decimal_places=2, null=True)
+    potential_earnings = DecimalField(decimal_places=2, null=True)
+    total_amount = DecimalField(decimal_places=2, null=True)
+    bet_status = CharField(max_length=64, null=True)
+    description = CharField(max_length=256, null=True)
+    create_datetime = DateTimeField(null=False)
+    finish_datetime = DateTimeField(null=True)
+    operator_fee = DecimalField(decimal_places=2, null=True)
+
+    class Meta:
+        database = Database.connect
---
+++ src/utils/connection.py
@@ -0,0 +1,31 @@
+from os import environ
+
+from peewee import MySQLDatabase
+
+from set_config import get_settings
+
+
+def get_database_name() -> str:
+    database = get_settings()
+
+    if database["database"] in ("production", "staging"):
+        return environ.get("DATABASE_NAME")
+
+    return "db_test"
+
+
+def get_mysql_credentials() -> dict:
+    return dict(
+        host=environ.get("DATABASE_HOST"),
+        port=environ.get("DATABASE_PORT"),
+        database=get_database_name(),
+        username=environ.get("DATABASE_USER"),
+        passwd=environ.get("DATABASE_PASSWD")
+    )
+
+
+class Database:
+    credentials = get_mysql_credentials()
+    connect = MySQLDatabase(
+        **credentials
+    )
