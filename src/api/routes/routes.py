4531ae095cf7dcf35df95da3a6e6687bff6537cc
Abel Carvalho <64908368+abelbarreto-dev@users.noreply.github.com>
2023-05-11 12:13:24 -0300

    feat: create routes and adjust models and interest calculation (#4)
    * feat: created a routes file with a dict with that information
    
    * chore: created a model to bet status, added new routes and improve models

---
+++ src/api/routes/routes.py
@@ -0,0 +1,19 @@
+ROUTES = {
+    "post_login": "/login",
+    "post_login_auth": "/login/auth",
+    "post_single_bet": "/bet/single",
+    "post_multi_bet": "/bet/multi",
+    "patch_login_passwd": "/login/recovery",
+    "patch_single_bet": "/bet/single",
+    "patch_multi_bet": "/bet/multi",
+    "get_filter_single": "/bet/single/{date_from}/{date_to}",
+    "get_filter_multi": "/bet/multi/{date_from}/{date_to}",
+    "get_profits_single": "/bet/single/profits",
+    "get_profits_multi": "/bet/multi/profits",
+    "get_lost_single": "/bet/single/lost",
+    "get_lost_multi": "/bet/multi/lost",
+    "get_all_profits": "/bet/profits/all",
+    "get_all_lost": "/bet/lost/all",
+    "get_odds_success": "/bet/odds-success/{date_opp}",
+    "post_compound_interest": "/compound-interest"
+}
