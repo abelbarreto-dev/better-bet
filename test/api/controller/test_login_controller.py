5bbc7de0d8a51d6656994ea409663de58ef07ea5
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-06-02 16:24:49 -0300

    chore: login tests improved

--- test/api/controller/test_login_controller.py
+++ test/api/controller/test_login_controller.py
@@ -2,23 +2,27 @@ from src.utils.create_tables import drop_all_tables
 
 from fastapi.testclient import TestClient
 
-from test.mock.data_mock import DATA
-
 from src.api.routes.routes import ROUTES
 
 
 def test_post_login_success(client: TestClient) -> None:
+    drop_all_tables()
+
     created_login = client.post(
         ROUTES["post_login"],
-        json=DATA["login"]
+        json={
+            "player_name": "Abel Developer",
+            "username": "abel_dev",
+            "password": "ahjfnnjh44fuK5"
+        }
     )
 
     assert created_login.status_code == 204
 
-    drop_all_tables()
-
 
 def test_post_login_failure_empty_json(client: TestClient) -> None:
+    drop_all_tables()
+
     created_login = client.post(
         ROUTES["post_login"],
         json={}
@@ -26,18 +30,26 @@ def test_post_login_failure_empty_json(client: TestClient) -> None:
 
     assert created_login.status_code == 422
 
-    drop_all_tables()
-
 
 def test_post_login_failure_existing_login(client: TestClient) -> None:
+    drop_all_tables()
+
     created_login = client.post(
         ROUTES["post_login"],
-        json=DATA["login"]
+        json={
+            "player_name": "Abel Developer",
+            "username": "abel_dev",
+            "password": "ahjfnnjh44fuK5"
+        }
     )
 
     created_login = client.post(
         ROUTES["post_login"],
-        json=DATA["login"]
+        json={
+            "player_name": "Abel Developer",
+            "username": "abel_dev",
+            "password": "ahjfnnjh44fuK5"
+        }
     )
 
     assert created_login.status_code == 400
@@ -47,4 +59,95 @@ def test_post_login_failure_existing_login(client: TestClient) -> None:
         }
     }
 
+
+def test_login_checker_failure(client: TestClient) -> None:
     drop_all_tables()
+
+    invalid_login = {
+        "player_name": "Abel Developer",
+        "username": "abel_dev",
+        "password": " passwfjagfne4j3"
+    }
+
+    created_login = client.post(
+        ROUTES["post_login"],
+        json=invalid_login
+    )
+
+    assert created_login.status_code == 400
+
+
+def test_login_auth_success(client: TestClient) -> None:
+    drop_all_tables()
+
+    created_login = client.post(
+        ROUTES["post_login"],
+        json={
+            "player_name": "Abel Developer",
+            "username": "abel_dev",
+            "password": "ahjfnnjh44fuK5"
+        }
+    )
+
+    make_login = client.post(
+        ROUTES["post_login_auth"],
+        json={
+            "username": "abel_dev",
+            "password": "ahjfnnjh44fuK5"
+        }
+    )
+
+    assert created_login.status_code == 204
+    assert make_login.status_code == 200
+
+
+def test_login_auth_failure_checker(client: TestClient) -> None:
+    drop_all_tables()
+
+    created_login = client.post(
+        ROUTES["post_login"],
+        json={
+            "player_name": "Abel Developer",
+            "username": "abel_dev",
+            "password": "ahjfnnjh44fuK5"
+        }
+    )
+
+    login_auth = {
+        "username": "abel_dev",
+        "password": " adnfjn44i49jf"
+    }
+
+    make_login = client.post(
+        ROUTES["post_login_auth"],
+        json=login_auth
+    )
+
+    assert created_login.status_code == 204
+    assert make_login.status_code == 400
+
+
+def test_login_auth_failure(client: TestClient) -> None:
+    drop_all_tables()
+
+    created_login = client.post(
+        ROUTES["post_login"],
+        json={
+            "player_name": "Abel Developer",
+            "username": "abel_dev",
+            "password": "ahjfnnjh44fuK5"
+        }
+    )
+
+    login_auth = {
+        "username": "abel_dev",
+        "password": "adnfjn44i49jf"
+    }
+
+    make_login = client.post(
+        ROUTES["post_login_auth"],
+        json=login_auth
+    )
+
+    assert created_login.status_code == 204
+    assert make_login.status_code == 404
