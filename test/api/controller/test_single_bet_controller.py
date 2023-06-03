3a979cce8d1efbf2666cc060cdd8abad934444f4
abelbarreto-dev <abelzinho.developer@gmail.com>
2023-06-02 21:29:54 -0300

    test: filter single bet failure get

--- test/api/controller/test_single_bet_controller.py
+++ test/api/controller/test_single_bet_controller.py
@@ -230,3 +230,47 @@ def test_get_filter_single_success(client: TestClient) -> None:
     assert get_single_bet.status_code == 200
 
     drop_all_tables()
+
+
+def test_get_filter_single_failure(client: TestClient) -> None:
+    drop_all_tables()
+
+    created_login = client.post(
+        ROUTES["post_login"],
+        json={
+            "player_name": "Abel Developer",
+            "username": "abel_dev",
+            "password": "ahjfnnjh44fuK5"
+        }
+    )
+
+    created_single_bet = client.post(
+        ROUTES["post_single_bet"],
+        json={
+            "id_login": 1,
+            "odd": "1.25",
+            "value_invest": "250.00",
+            "description": "+0.5 goals",
+            "home_team": "Liverpool",
+            "away_team": "Manchester City",
+            "team_bet": None,
+            "operator_fee": "0.065"
+        }
+    )
+
+    brazil_date = str(get_datetime_brazil().date())
+
+    get_single_bet = client.post(
+        ROUTES["get_filter_single"],
+        json={
+            "login_id": 2,
+            "date_from": brazil_date,
+            "date_to": brazil_date
+        }
+    )
+
+    assert created_login.status_code == 204
+    assert created_single_bet.status_code == 204
+    assert get_single_bet.status_code == 404
+
+    drop_all_tables()
